c45badca0e7d47d7d6a0a1ec559814012a1b63e3
Livya Branco <livyabranco@gmail.com>
2022-05-31 17:22:18 +0200

    seed is done

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :user
 
-  validates :title, :type, :address, :description, :date, :language, :capacity, presence: true
-  validates :type, inclusion: { in: ["Dinner", "Cooking Class", "Food Tour", "Brunch"] }
+  validates :title, :category, :address, :description, :date, :language, :capacity, presence: true
+  validates :category, inclusion: { in: ["Dinner", "Cooking Class", "Food Tour", "Brunch"] }
   validates :language, inclusion: { in: ["French", "English", "Spanish", "Portuguese"] }
 end
