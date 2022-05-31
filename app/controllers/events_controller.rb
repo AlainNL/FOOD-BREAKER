242a0877df65316035d3186ea6d293a11c3c256c
Alain Noel <alain.noel34@gmail.com>
2022-05-31 18:23:16 +0200

    index form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,14 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.all
+    if params[:query].present?
+      sql_query = <<~SQL
+      events.address ILIKE :query
+      OR events.language ILIKE :query
+      OR events.date ILIKE :query
+      SQL
+      @events = Event.joins(:users).where(sql_query, query: "%#{params[:query]}%")
+    else
+      @events = Event.all
+    end
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,6 @@ class Event < ApplicationRecord
   validates :title, :category, :address, :description, :date, :language, :capacity, presence: true
   validates :category, inclusion: { in: ["Dinner", "Cooking Class", "Food Tour", "Brunch"] }
   validates :language, inclusion: { in: ["French", "English", "Spanish", "Portuguese"] }
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
