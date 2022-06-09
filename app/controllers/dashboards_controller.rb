c99f3d2835563a55231942ca1211379bc6c32773
Livya Branco <livyabranco@gmail.com>
2022-06-09 19:18:15 +0200

    update dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -7,5 +7,6 @@ class DashboardsController < ApplicationController
     @mybookings = @user.bookings.map {|booking| booking.event}
     @my_futur_bookings = @mybookings.select {|event| event.date >= Time.now}
     @my_past_bookings = @mybookings.select {|event| event.date < Time.now}
+    @my_past_events = @user.events.where("date < ?", Time.now)
   end
 end
