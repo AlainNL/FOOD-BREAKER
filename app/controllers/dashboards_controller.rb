94afb0dc2377301be34bc41d19de1bafc75490a3
Emmanuelle Amouroux <amouroux.emmanuelle@orange.fr>
2022-06-10 08:29:43 +0200

    correction sur le master local

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,9 +4,9 @@ class DashboardsController < ApplicationController
   def dashboard
     @user = current_user
     @myevents = @user.events.order("events.date ASC")
-    @mybookings = @user.bookings.map {|booking| booking.event}
-    @my_futur_bookings = @mybookings.select {|event| event.date >= Time.now}
-    @my_past_bookings = @mybookings.select {|event| event.date < Time.now}
+    @mybookings = @user.bookings.map { |booking| booking.event }
+    @my_futur_bookings = @mybookings.select { |event| event.date >= Time.now }
+    @my_past_bookings = @mybookings.select { |event| event.date < Time.now }
     @my_past_events = @user.events.where("date < ?", Time.now)
   end
 end
